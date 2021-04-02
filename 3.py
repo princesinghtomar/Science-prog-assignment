ec7df3c9ede52235cf4279a4958708ee713be3ed
princesinghtomar <singhtomarprince470@gmail.com>
2021-04-02 16:08:13 +0530

    optimised

--- 3.py
+++ 3.py
@@ -6,20 +6,38 @@ print(sequence)
 
 rec_sites = []
 
+def ad_palindrome(k,j,diff):
+    rec_string = ""
+    while(j<len(sequence) and k>=0 and sequence[k] == dnacompliment[sequence[j]]):
+        rec_string = sequence[k] + rec_string + sequence[j]
+        k-=1
+        j+=1
+    if(j-k > diff ):
+        rec_sites.append(rec_string)
+
 for i in range(len(sequence)):
     if(sequence[i]!='a' and sequence[i]!='t' and sequence[i]!='c' and sequence[i]!='g'):
         print("> Enter Correct DNA Strand")
         exit()
     else:
-        rec_string = ""
         k = i
         j = i+1
-        while(j<len(sequence) and k>=0 and sequence[k] == dnacompliment[sequence[j]]):
-            rec_string = sequence[k] + rec_string + sequence[j]
-            k-=1
-            j+=1
-        if(j-k > 3 ):
-            rec_sites.append(rec_string)
+        ad_palindrome(i,i+1,3)
+        # while(j<len(sequence) and k>=0 and sequence[k] == dnacompliment[sequence[j]]):
+        #     rec_string = sequence[k] + rec_string + sequence[j]
+        #     k-=1
+        #     j+=1
+        # if(j-k > 3 ):
+        #     rec_sites.append(rec_string)
+        # k=i-1
+        # j=i+1
+        ad_palindrome(i-1,i+1,7)
+        # while(j<len(sequence) and k>=0 and sequence[k] == dnacompliment[sequence[j]]):
+        #     rec_string = sequence[k] + rec_string + sequence[j]
+        #     k-=1
+        #     j+=1
+        # if(j-k > 7 ):
+        #     rec_sites.append(rec_string)
 
 print("len(rec_sites) : " + str(len(rec_sites)) )
 if(len(rec_sites) > 0):
